fe56e6e1db8122f4a1b87c0e7a547a86afd752a0
John Deligiannis <john.deligiannis1@gmail.com>
2023-01-15 16:18:41 +0200

    added all sections, plus emailjs functionality

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Jambotron from './Jambotron';
 import Work from './Work';
 import GetProjects from './GetProjects.js';
 import Showcase from './Showcase';
+import Skills from './Skills';
+import Contact from './Contact';
 
 const App = () => {
-  const [showcaseProject, setShowcaseProject] = useState("");
-  return (
-    <div className="App">
-      <Navbar />
-      <Jambotron />
-      <Work projects={GetProjects()} setShowcaseProject={setShowcaseProject} />
-      {(showcaseProject !== "") ? <Showcase projects={GetProjects()} showcaseProject={showcaseProject} setShowcaseProject={setShowcaseProject} /> : []}
-    </div>
-  );
+	const [showcaseProject, setShowcaseProject] = useState("");
+	return (
+		<div className="App">
+			<Navbar />	
+			<Jambotron />
+			<Work projects={GetProjects()} setShowcaseProject={setShowcaseProject} />
+			{(showcaseProject !== "") ? <Showcase projects={GetProjects()} showcaseProject={showcaseProject} setShowcaseProject={setShowcaseProject} /> : []}
+			<Skills />
+			<Contact />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/Skills.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+	return (
+		<div className="Skills section-container" id="work">
+			<div className="section-title">
+				<hr /><p>Skills</p>
+			</div>
+			<div className="skills">
+				<div className="about">
+					<div className="about-title">About me</div>
+					<p>
+						Recent graduate with an Integrated Master's Degree in Informatics and Computer
+						Engineering having a specialty in Software Engineering.
+					</p>
+					<p>
+						Highly motivated to work as a software engineer, eager to learn and follow
+						software design patterns and work with new technologies and solutions.
+					</p>
+					<p>
+						With background knowledge including analysis, design and development of 
+						information systems written in Java and web-based applications developed
+						using HTML, CSS, React, REST APIs and MySQL.
+					</p>
+				</div>
+				<div className="categories">
+					<div className="category">
+						<i className="fa fa-code" />
+						<p className="title">Front-End</p>
+						<fieldset>
+							<legend>Languages</legend>
+							<ul>
+								<li><p>HTML5</p></li>
+								<li><p>CSS3</p></li>
+								<li><p>Javascript</p></li>
+							</ul>
+						</fieldset>
+						<fieldset>
+							<legend>Frameworks</legend>
+							<ul>
+								<li><p>React</p></li>
+							</ul>
+						</fieldset>
+					</div>
+					<hr />
+					<div className="category">
+						<i className="fa fa-cogs" />
+						<p className="title">Back-End</p>
+						<fieldset>
+							<legend>Languages</legend>
+							<ul>
+								<li>Java</li>
+								<li>PHP</li>
+								<li>MySQL</li>
+							</ul>
+						</fieldset>
+						<fieldset>
+							<legend>Technologies</legend>
+							<ul>
+								<li>REST API</li>
+								<li>Spring</li>
+								<li>JPA</li>
+								<li>Hibernate</li>
+								<li>Maven</li>
+								<li>JWT</li>
+								<li>JSP</li>
+								<li>Servlet</li>
+							</ul>
+						</fieldset>
+					</div>
+					<hr />
+					<div className="category">
+						<i className="fa fa-asterisk" />
+						<p className="title">Other</p>
+						<fieldset>
+							<legend>Versioning Systems</legend>
+							<ul>
+								<li>Github</li>
+								<li>Git</li>
+							</ul>
+						</fieldset>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Skills;
