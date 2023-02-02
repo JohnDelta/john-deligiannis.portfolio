e51201963a5ab87a687eb258e0d5d8d78c068b2c
John Deligiannis <john.deligiannis1@gmail.com>
2023-02-02 15:15:25 +0200

    update about section

--- src/Skills.js
+++ src/Skills.js
@@ -11,19 +11,19 @@ const Skills = () => {
 				<div className="about">
 					<div className="about-title">About me</div>
 					<p>
-						John has an <strong>Integrated Master's Degree in Informatics and Computer
-						Engineering</strong> with a specialty in <strong>Software Engineering</strong>. He has worked as a <strong>full-stack developer</strong> and contributed to the design, development and testing 
+						I have an <strong>Integrated Master's Degree in Informatics and Computer
+						Engineering</strong> with a specialty in <strong>Software Engineering</strong>. I've worked as a <strong>full-stack developer</strong> and contributed to the design, development and testing 
 						of large projects for a real-world, web-based application. 
 					</p>
 					<p>
-						He is highly motivated to work as a software engineer, eager to learn and follow
+						I am highly motivated to work as a software engineer, eager to learn and follow
 						software design principles and patterns, and work on new technologies and solutions.
 					</p>
 					<p>
-						From his work experience and personal projects, he has developed strong fundamentals in
-						software development and he acquired knowledge in <strong>OOP</strong> principles with <strong>Java</strong> and <strong>C#</strong>. 
-						SQL and NoSQL Databases with <strong>MySQL</strong> and <strong>Microsoft SQL</strong>. Knowledge of web protocols
-						and technologies. Web applications that follow <strong>MVC</strong> and <strong>Microservices</strong> designs built with <strong>Java</strong>, <strong>Jsp</strong>, <strong>Spring Boot</strong>, <strong>Spring MVC</strong> and <strong>React</strong>.
+						From my work experience and personal projects, I have developed strong fundamentals in
+						software development and I acquired knowledge in <strong>OOP</strong> principles with <strong>Java</strong> and <strong>C#</strong>. 
+						Worked on SQL Databases with <strong>MySQL</strong> and <strong>Microsoft SQL</strong>. Utilized ORM frameworks like <strong>Entity Framework</strong>. I have Knowledge of web protocols
+						and technologies. Web applications that follow <strong>MVC</strong> and <strong>Microservices</strong> designs built with <strong>Spring Boot</strong> and <strong>React</strong>.
 						Worked on web-based solutions with <strong>ASP.NET WebForms</strong>, <strong>VB.NET</strong>, <strong>DevExpress</strong>, <strong>HTML</strong>, <strong>CSS</strong>, <strong>Javascript</strong>, <strong>JQuery</strong>.
 						Experienced with source control systems <strong>(Git)</strong> and hosting services <strong>(GitHub)</strong>.
 						Understanding of <strong>unit and integration testing</strong> concepts.
@@ -62,15 +62,11 @@ const Skills = () => {
 							</ul>
 						</fieldset>
 						<fieldset>
-							<legend>Technologies</legend>
+							<legend>Frameworks</legend>
 							<ul>
-								<li>RESTful API</li>
-								<li>ASP.NET WebForms</li>
-								<li>Spring MVC</li>
-								<li>JPA</li>
-								<li>Maven</li>
-								<li>JWT</li>
-								<li>JSP</li>
+								<li>ASP.NET (WebForms and Web API)</li>
+								<li>Spring Boot (for RESTFul APIs)</li>
+								<li>Entity Framework</li>
 							</ul>
 						</fieldset>
 					</div>
